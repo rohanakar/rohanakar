b766ae5bb50b373a8b8afe24ceda6419b7048670
rishabh <rishabhc329@gmail.com>
2020-08-14 18:56:16 +0530

    updated readme

---
+++ index.js
@@ -0,0 +1,26 @@
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+let DATA = {
+  name: 'Rishabh',
+  date: new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'Asia/Kolkata',
+  }),
+};
+/**
+  * A - We open 'main.mustache'
+  * B - We ask Mustache to render our file with the data
+  * C - We create a README.md file with the generated output
+  */
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+generateReadMe();
